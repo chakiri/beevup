84a6407930245de782f37458afe824c0f80f8e1f
MOHAMED CHAKIRI <yassir.chakiri12@gmail.com>
2020-03-11 11:46:06 +0100

    fix bug 2

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    
 ];
