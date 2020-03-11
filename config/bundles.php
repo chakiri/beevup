e18bfba8d043fafc6906375bdea81d0dcbd7d88d
Nihel-Loukil <nihel.loukil@bureau-vallee.com>
2020-03-11 10:01:00 +0100

    delete unused package

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    
 ];
