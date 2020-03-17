aea60fc1887bca2851f231d1516f625f7de89bb7
Nihel-Loukil <nihel.loukil@bureau-vallee.com>
2020-03-17 14:31:44 +0100

    delete unued dump

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -73,8 +73,6 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
-        dump($error );
-
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render("security/login.html.twig", [
