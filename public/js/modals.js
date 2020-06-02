bd965577245fffc86dede5ade9883c7b81a827fc
MOHAMED CHAKIRI <yassir.chakiri12@gmail.com>
2020-06-02 12:02:11 +0200

    checkbox modal intro

--- public/js/modals.js
+++ public/js/modals.js
@@ -13,6 +13,20 @@ $('.prev').click(function(){
     prevModal($(this))
 });
 
+$('input[name=onboarding]').click(function () {
+    var value = $(this).is(':checked');
+    setOnborading(value);
+    //Change all checkboxes value
+    $('input[name=onboarding]').each(function(){
+        $(this).prop('checked', value);
+    });
+});
+
+$('#startIntro').click(function () {
+    hideModal($(this));
+    startIntro();
+});
+
 function nextModal(e){
     var modalNum = e.closest("#welcomeModals").data('num');
     $('#welcomeModal'+modalNum).modal('hide');
@@ -25,4 +39,33 @@ function prevModal(e){
     $('#welcomeModal'+modalNum).modal('hide');
     var prevModal = modalNum - 1;
     $('#welcomeModal'+prevModal).modal('show');
+}
+
+function hideModal(e){
+    var modalNum = e.closest("#welcomeModals").data('num');
+    $('#welcomeModal'+modalNum).modal('hide');
+}
+
+function setOnborading(value){
+    var url = document.getElementById("onboarding").dataset.url;
+    $.ajax({
+        type: 'post',
+        url: url,
+        data: {
+            value: value
+        }
+    });
+}
+
+//Intro Js
+function startIntro(){
+    var intro = introJs();
+    /*intro.onbeforechange(function () {
+     //Get number before last step (4)
+     if (this._currentStep === 4) {
+     setOnborading();
+     }
+     });*/
+    intro.setOptions({ 'skipLabel': 'Passer', 'doneLabel': 'Terminer', 'prevLabel': 'Précédent', 'nextLabel': 'Suivant' });
+    intro.start();
 }
