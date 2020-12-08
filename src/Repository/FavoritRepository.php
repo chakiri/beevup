88fabaaa0d8b72e5a9de11c8e8d03e1830169430
yassir <yassir.chakiri12@gmail.com>
2020-12-08 13:52:04 +0100

    remove common from ManagerRegistry

--- src/Repository/FavoritRepository.php
+++ src/Repository/FavoritRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Favorit;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Favorit|null find($id, $lockMode = null, $lockVersion = null)
--- src/Repository/RecommandationRepository.php
+++ src/Repository/RecommandationRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Recommandation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\ServiceRepository;
 /**
  * @method Recommandation|null find($id, $lockMode = null, $lockVersion = null)
