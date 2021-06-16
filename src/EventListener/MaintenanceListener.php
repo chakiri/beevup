cc73c6701eff42444ff4c8c1fd2c9883a30bdee6
yassir <yassir.chakiri12@gmail.com>
2021-06-16 13:39:47 +0200

    fix mainteance

--- src/EventListener/MaintenanceListener.php
+++ src/EventListener/MaintenanceListener.php
@@ -2,18 +2,18 @@
 
 namespace App\EventListener;
 
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Twig\Environment;
 
 class MaintenanceListener
 {
-    private $container, $maintenance, $ipAuthorized, $twig, $event;
+    private $maintenance;
+    private $ipAuthorized;
+    private $twig;
 
-    public function __construct($maintenance, ContainerInterface $container,  Environment $twig)
+    public function __construct($maintenance,  Environment $twig)
     {
-        $this->container = $container;
         $this->maintenance = $maintenance["statut"];
         $this->ipAuthorized = $maintenance["ipAuthorized"];
         $this->twig = $twig;
@@ -25,8 +25,7 @@ class MaintenanceListener
         $currentIP = $_SERVER['REMOTE_ADDR'];
         if ($maintenance AND !in_array($currentIP, $this->ipAuthorized)) {
             // We load our maintenance template
-
-            $template =  $this->twig->render('maintenance/maintenance.html.twig');
+            $template =  $this->twig->render('maintenance/maintenance.html.twig', ['maintenance' => true]);
             // We send our response with a 503 response code (service unavailable)
             $event->setResponse(new Response($template, 503));
             $event->stopPropagation();
